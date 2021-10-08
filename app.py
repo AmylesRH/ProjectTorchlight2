b1e5328c5ad335fef681e1c8813dbf4c73fea9d3
AmylesRH <88393013+AmylesRH@users.noreply.github.com>
2021-10-08 05:32:35 -0400

    update

--- app.py
+++ app.py
@@ -6,32 +6,31 @@ from mysql.connector import connect, Error
 app = Flask(__name__)
 
 
-try:
-    with connect(
-        host="localhost",
-        user=input("Enter username: "),
-        password=getpass("Enter password: "),
-        database="ccsdataplatformdb",
-    ) as connection:
-        print(connection)
-except Error as e:
-    print(e)
+def connect():
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='https://ccsdata.apps.ocp4.prod.psi.redhat.com',
+                                       database='ccsdataplatformdb',
+                                       user='userBO1',
+                                       password=getpass("Enter password: ")
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
 
 if __name__ == '__main__':
-    app.run()
+    connect()
 
-@app.route('/')
-def main():
-    """Entry point; the view for the main page"""
-    cities = [(record.city_id, record.city_name) for record in data]
-    return render_template('main.html', cities=cities)
-
-
-
-select_rhel = """
+select_product = """
  SELECT *
  FROM WebBehavior
- WHERE product_facet = 'rhel'
  ORDER BY findability DESC
  """
 with connection.cursor() as cursor:
@@ -39,27 +38,25 @@ with connection.cursor() as cursor:
      for row in cursor.fetchall():
          print(row)
 
-
-
-
+@app.route('/')
+def main():
+    """Entry point; the view for the main page"""
+return render_template('main.html')
 
 @app.route('/productview')
 def productview():
     """The view for the Product Views page"""
-    cities = [(record.city_id, record.city_name) for record in data]
-    return render_template('productview.html', cities=cities)
+    return render_template('productview.html')
 
 @app.route('/contenttype')
 def contenttype():
-    """The view for the Product Views page"""
-    cities = [(record.city_id, record.city_name) for record in data]
-    return render_template('contenttype.html', cities=cities)
+    """The view for the Content Type page"""
+    return render_template('contenttype.html')
 
 @app.route('/pageview')
 def pageview():
-    """The view for the Product Views page"""
-    cities = [(record.city_id, record.city_name) for record in data]
-    return render_template('pageview.html', cities=cities)
+    """The view for the Page Views page"""
+    return render_template('pageview.html')
 
 
 
